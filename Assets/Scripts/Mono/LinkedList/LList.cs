83f4d46ae765a3365027947a2619667f8726a59e
Victor Doktare <victor@doktare.com>
2021-11-10 15:20:53 +0100

    Basic LList Structure working

--- Assets/Scripts/Mono/LinkedList/LList.cs
+++ Assets/Scripts/Mono/LinkedList/LList.cs
@@ -1,4 +1,5 @@
 using System;
+using UnityEngine;
 
 namespace Mono.LinkedList
 {
@@ -6,30 +7,19 @@ namespace Mono.LinkedList
     {
 
         private Node _head;
-        private int _count;
 
+        //Access
         public Node Next => _head.Next;
-        public Node GetHead => _head;
-        public Node GetTail => GetLast();
-        public int Count
-        {
-            get
-            {
-                if (_count < 0)
-                {
-                    _count = 0;
-                }
-                
-                return _count;
-            }
-            private set => _count = value;
-        }
+        public Node Head => _head;
+        public Node Tail => GetLast();
+        public int Count => GetCount();
+
 
         //Add Nodes
         public void AddFirst(T type)
         {
             var newNode = new Node(type);
-            Count++;
+            //Count++;
 
             if (_head != null)
             {
@@ -41,7 +31,7 @@ namespace Mono.LinkedList
         public void AddLast(T type)
         {
             var newNode = new Node(type);
-            Count++;
+            //Count++;
             
             if (_head == null)
             {
@@ -51,36 +41,137 @@ namespace Mono.LinkedList
             var lastNode = GetLast();
             lastNode.Next = newNode;
         }
+        public void AddAfter(Node prevNode, T type)
+        {
+            var node = new Node(type);
+            node.Next = prevNode.Next;
+            prevNode.Next = node;
+        }
         
         //Remove Nodes
         public void RemoveFirst()
         {
             var node = _head;
-            Count--;
+            //Count--;
             
             if (node == null)
             {
-                throw new NullReferenceException("LList has no head");
+                throw new NullReferenceException("LList is empty");
             }
 
             _head = _head.Next;
         }
-
-        //Todo Remove Last
         public void RemoveLast()
         {
             var node = _head;
-            Count--;
+            //Count--;
 
-            if (node != null)
+            if (node == null)
             {
-                while (node.Next.Next != null)
-                {
-                    node = node.Next;
-                }
+                throw new NullReferenceException("LList is empty");
+            }
+
+            while (node.Next.Next != null)
+            {
+                node = node.Next;
+            }
+
+            node.Next = null;
+
+        }
+        public void RemoveAt(int index)
+        {
+            var node = _head;
+            //Count--;
+
+            if (node == null)
+            {
+                throw new NullReferenceException("LList is empty");
+            }
+
+            if (index == 0)
+            {
+                _head = node.Next;
+                return;
+            }
+
+            for (int i = 0; node != null && i < index - 1; i++)
+            {
+                node = node.Next;
+            }
+
+            if (node == null || node.Next == null)
+            {
+                return;
+            }
+
+            var next = node.Next.Next;
+            node.Next = next;
+        }
+
+        //Useful Functions
+        public Node GetIndex(int index)
+        {
+            var node = _head;
+            
+            if (node == null)
+            {
+                throw new NullReferenceException("LList is empty");
+            }
+
+            for (int i = 0; node != null && i < index; i++)
+            {
+                node = node.Next;
+            }
+
+            if (node == null)
+            {
+                throw new NullReferenceException("LList index out of range");
+            }
+            
+            return node;
+        }
+        private int GetCount()
+        {
+            var node = _head;
+            int count = 0;
+
+            while (node != null)
+            {
+                count++;
+                node = node.Next;
+            }
+
+            return count;
+        }
+        public void PrintList()
+        {
+            var node = _head;
+
+            if (node == null)
+            {
+                throw new NullReferenceException("LList is empty");
+            }
+            
+            while (node != null)
+            {
+                Debug.Log(node.Data);
+                node = node.Next;
             }
         }
 
+        //Node
+        public class Node
+        {
+            public T Data { get; set; }
+            public Node Next { get; set; }
+        
+            public Node(T type)
+            {
+                Data = type;
+                Next = null;
+            }
+        }
         private Node GetLast()
         {
             var node = _head;
@@ -92,19 +183,5 @@ namespace Mono.LinkedList
 
             return node;
         }
-        
-        public class Node
-        {
-            public T Value { get; set; }
-            public Node Next { get; set; }
-        
-            public Node(T type)
-            {
-                Value = type;
-                Next = null;
-            }
-        }
-    
-    
     }
 }
--- Assets/Scripts/Mono/Player/PlayerBody.cs
+++ Assets/Scripts/Mono/Player/PlayerBody.cs
@@ -1,35 +1,34 @@
 using System.Collections.Generic;
 using Mono.LinkedList;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class PlayerBody : MonoBehaviour
 {
     private LList<int> _llBody;
-    private List<int> _test;
 
     private void Start()
+    {
+        LinkListDebugging();
+    }
+    
+    private void LinkListDebugging()
     {
         _llBody = new LList<int>();
 
         var one = 1;
         var two = 2;
         var three = 3;
-
+        var four = 4;
 
         _llBody.AddFirst(one);
-         _llBody.AddLast(two);
-         _llBody.AddLast(three);
-
-        Debug.Log(_llBody.Count);
-        Debug.Log($"Head.Next: {_llBody.Next.Value}");
-        Debug.Log($"Head: {_llBody.GetHead.Value}  Tail: {_llBody.GetTail.Value}");
-        
-        _llBody.RemoveFirst();
+        _llBody.AddLast(two);
+        _llBody.AddLast(three);
         
         Debug.Log(_llBody.Count);
-        Debug.Log($"Head.Next: {_llBody.Next.Value}");
-        Debug.Log($"Head: {_llBody.GetHead.Value}  Tail: {_llBody.GetTail.Value}");
-
+        Debug.Log($"Node at index 0 value: {_llBody.GetIndex(0).Data}");
+        Debug.Log($"Node at index 1 value: {_llBody.GetIndex(1).Data}");
+        Debug.Log($"Node at index 2 value: {_llBody.GetIndex(2).Data}");
+        Debug.Log($"Node at index 2 value: {_llBody.GetIndex(3).Data}");
+        
     }
 }
